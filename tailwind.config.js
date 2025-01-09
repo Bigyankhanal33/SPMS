f3e12ab53db64e93edda45d659d1f93a65a91fb6
pradattaaryal <pradattaaryal2468@gmail.com>
2025-01-09 14:37:34 +0545

    Updated Tailwind config and index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,29 @@ export default {
     extend: {
       fontFamily: {
         dm: ['"DM Sans"', 'sans-serif'],
+        roboto: ['"Roboto"', 'sans-serif'],
       },
       fontSize: {
         'custom-36': '36px',
+        'custom-28':'28px',
+        'custom-18':'18px',
+        'custom-btn':'20px',
+
       },
       fontWeight: {
         bold: '700',
+        medium:'500',
+        regular:'400'
+
+      },
+      colors: {
+        black: '#030303',  
+        secondary: '#193960',  
+        primary: '#2475A5',  
+        white: '#FEFEFE',  
+        background: '#EDF2F6',  
+        hover: '#3279A5', 
+        fade_white: '#FBFBFB',   
       },
     },
   },
